d2190ea865e8cb6a19ab745ae98812c56495e7cd
Alexander Horn <alexhorn@users.noreply.github.com>
2021-09-08 22:23:06 +0200

    Send welcome email automatically

--- ldap/create-ldap-user.js
+++ ldap/create-ldap-user.js
@@ -1,6 +1,9 @@
 const readline = require('readline')
 const ldap = require('ldapjs')
+const nodemailer = require('nodemailer')
+const mustache = require('mustache')
 const passwordGenerator = require('generate-password')
+const { readFileSync } = require('fs')
 const { promisify } = require('util')
 
 require('dotenv').config()
@@ -12,10 +15,20 @@ const rl = readline.createInterface({
 rl.questionAsync = question => new Promise(resolve => rl.question(question, resolve))
 
 const ldapClient = ldap.createClient({
-  url: process.env.SERVER
+  url: process.env.LDAP_SERVER
 })
 ldapClient.bindAsync = promisify(ldapClient.bind)
 ldapClient.addAsync = promisify(ldapClient.add)
+
+const transporter = nodemailer.createTransport({
+  host: process.env.SMTP_HOST,
+  port: +process.env.SMTP_PORT,
+  secure: false,
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS
+  }
+})
 
 function normalize (str) {
   return str.trim()
@@ -24,41 +37,66 @@ function normalize (str) {
     .replace(/ö/g, 'oe')
     .replace(/ü/g, 'ue')
     .replace(/ß/g, 'ss')
+    .replace(/ /g, '.')
+}
+
+async function createLdapLogin (firstName, lastName) {
+  await ldapClient.bindAsync(process.env.LDAP_BIND_CN, process.env.LDAP_BIND_PASSWORD)
+
+  try {
+    const cn = `${normalize(firstName)}.${normalize(lastName)}`
+    const dn = `cn=${cn},${process.env.LDAP_USER_CN}`
+    const email = `${cn}@neuland-ingolstadt.de`
+    const password = passwordGenerator.generate({ length: 12, lowercase: true, uppercase: true, numbers: true })
+
+    console.log('Creating LDAP account ...')
+    const entry = {
+      cn: cn,
+      givenName: firstName,
+      sn: lastName,
+      displayName: `${firstName} ${lastName}`,
+      userPassword: password,
+      mail: email,
+      mailEnabled: 'TRUE',
+      objectClass: [
+        'inetOrgPerson',
+        'PostfixBookMailAccount'
+      ]
+    }
+    await ldapClient.addAsync(dn, entry)
+
+    return { email, password }
+  } finally {
+    ldapClient.destroy()
+  }
+}
+
+async function sendWelcomeEmail (privateEmail, firstName, lastName, email, password) {
+  const body = mustache.render(readFileSync('email.mu', 'utf-8'), { firstName, lastName, email, password })
+  console.log('Sending email ...')
+  await transporter.sendMail({
+    from: 'Neuland Ingolstadt e.V. <noreply@neuland-ingolstadt.de>',
+    to: `${firstName} ${lastName} <${privateEmail}>`,
+    subject: 'Willkommen bei Neuland Ingolstadt',
+    html: body
+  })
 }
 
 async function main () {
-  await ldapClient.bindAsync(process.env.BIND_CN, process.env.BIND_PASSWORD)
-
   console.log('Collecting information for LDAP account ...')
   const firstName = (await rl.questionAsync('First name: ')).trim()
   const lastName = (await rl.questionAsync('Last name: ')).trim()
+  const privateEmail = (await rl.questionAsync('Private Email: ')).trim()
 
-  const cn = `${normalize(firstName)}.${normalize(lastName)}`
-  const dn = `cn=${cn},${process.env.USER_CN}`
-  const email = `${cn}@neuland-ingolstadt.de`
-  const password = passwordGenerator.generate({ length: 12, lowercase: true, uppercase: true, numbers: true })
-
-  console.log('Creating LDAP account ...')
-  const entry = {
-    cn: cn,
-    givenName: firstName,
-    sn: lastName,
-    displayName: `${firstName} ${lastName}`,
-    userPassword: password,
-    mail: email,
-    mailEnabled: 'TRUE',
-    objectClass: [
-      'inetOrgPerson',
-      'PostfixBookMailAccount'
-    ]
-  }
-  await ldapClient.addAsync(dn, entry)
+  const { email, password } = await createLdapLogin(firstName, lastName)
+  //const { email, password } = { email: 'test@example.com', password: 'yeet' }
 
   console.log('LDAP account created.')
-  console.log(`Email: ${email}`)
-  console.log(`Password: ${password}`)
 
-  ldapClient.destroy()
+  await sendWelcomeEmail(privateEmail, firstName, lastName, email, password)
+
+  console.log('Welcome email sent.')
+
   rl.close()
 }
 
---
+++ ldap/email.mu
@@ -0,0 +1,42 @@
+<html>
+    <head>
+    </head>
+    <body>
+        <p>
+            Hallo {{firstName}},
+        </p>
+        <p>
+            herzlich willkommen bei Neuland Ingolstadt. :)
+        </p>
+        <p>
+            Hier sind deine Neuland-Zugangsdaten:
+        </p>
+        <p>
+            <strong>E-Mail:</strong> {{email}}<br>
+            <strong>Passwort:</strong> {{password}}
+        </p>
+        <p>
+            Hier findest du eine Übersicht über unsere Dienste:
+            <a href="https://pad.informatik.sexy/Onboarding">Onboarding</a>
+        </p>
+        <p>
+            Wir kommunizieren über eine <strong>Signal-Chatgruppe</strong>.
+            Hier kannst du unserer Gruppe beitreten:
+            <a href="https://signal.group/#CjQKIJuYv3MToYxwinSiy0dBcELEHBd5ABfjxPnAeJTvouUjEhCWu3aGb0C5fqxfiJBjs7-l">Signal-Gruppe beitreten</a>
+        </p>
+        <p>
+            Es findet in regelmäßigen Abständen ein Neuland-Stammtisch statt.
+            Der Termin und der Ort wird immer über die Signal-Gruppe angekündigt.
+            Wir würden uns freuen, wenn du mal vorbeischaust.
+        </p>
+        <p>
+            Bis dann!
+        </p>
+        <p>
+            Viele Grüße
+        </p>
+        <p>
+            Manu, Quirin und Alex
+        </p>
+    </body>
+</html>
